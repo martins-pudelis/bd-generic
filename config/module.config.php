0b284a9a7b55ec53d6d344af97536f5d0266f2c3
martins.pudelis@wallcity.lv <martins.pudelis@wallcity.lv>
2014-06-11 01:07:54 +0300

    Generic module code update

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,15 @@
 <?php
 namespace BdGeneric;
 
-return array();
+return array(
+    'bd_generic_config' => array(
+        'email' => 'mach@inbox.lv',
+        'name' => 'Base BD',
+    ),
+
+    'service_manager' => array(
+        'factories' => array(
+            'navigation' => 'Zend\Navigation\Service\DefaultNavigationFactory',
+        ),
+    ),
+);
---
+++ src/BdGeneric/Service/EmailService.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace BdGeneric\Service;
